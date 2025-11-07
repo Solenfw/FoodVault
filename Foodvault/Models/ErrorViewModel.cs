f41aa0ed8a47ecc5754d6641c8c9e95017ccee77
Solenfw <vincentcody0175@gmail.com>
2025-11-07 14:26:57 +0700

    add .gitignore and rm tracking

---
+++ Foodvault/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Foodvault.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
